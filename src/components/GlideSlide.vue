f30717f51fe4af2e6fb91cda1c476d9d71dd4b33
Anton Reshetov <reshetov.art@gmail.com>
2018-07-30 06:51:50 +0300

    Add: Glide slide component

---
+++ src/components/GlideSlide.vue
@@ -0,0 +1,22 @@
+<template>
+  <li
+    class="glide__slide"
+    :data-glide-index="index">
+    <slot></slot>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'VueGlideSlide',
+
+  computed: {
+    /**
+     * Get index of current slide
+     */
+    index () {
+      return this.$parent.$children.findIndex(slide => slide._uid === this._uid)
+    }
+  }
+}
+</script>
