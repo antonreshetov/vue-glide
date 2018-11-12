3e8c1cd44b4f2d5f7b5e971987b4808612547515
Anton Reshetov <reshetov.art@gmail.com>
2018-11-12 14:11:49 +0300

    Docs: Update build

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,6 +4,12 @@ module.exports = {
   editLinks: true,
   themeConfig: {
     nav: [
+      {
+        text: 'Other project',
+        items: [
+          { text: 'Email signature generator', link: 'https://mysigmail.com' }
+        ]
+      },
       { text: 'GitHub', link: 'https://github.com/antonreshetov/vue-glide' }
     ],
     sidebar: ['/', '/getting-started/']
