26ce8c63d497c22e104b88e57e2594dc6a14d3d3
Anton Reshetov <reshetov.art@gmail.com>
2018-12-07 09:11:33 +0300

    Chore(Docs): add carbon ad

---
+++ docs/.vuepress/theme/CarbonAd.vue
@@ -0,0 +1,60 @@
+<script>
+export default {
+  render (h) {
+    return h('div', { class: 'carbon' })
+  },
+  mounted () {
+    this.load()
+  },
+  watch: {
+    $route (to, from) {
+      if (
+        to.path !== from.path &&
+        this.$el.querySelector('#carbonads')
+      ) {
+        this.$el.innerHTML = ''
+        this.load()
+      }
+    }
+  },
+  methods: {
+    load () {
+      const s = document.createElement('script')
+      s.id = '_carbonads_js'
+      s.src = `//cdn.carbonads.com/carbon.js?serve=CK7DEK7I&placement=antonreshetovgithubio`
+      this.$el.appendChild(s)
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+.carbon {
+  min-height: 102px;
+  padding: 1.5rem 1.5rem 0;
+  margin-bottom: -0.5rem;
+  font-size: 0.75rem;
+
+  a {
+    color: #444;
+    font-weight: normal;
+    display: inline !important;
+  }
+
+  .carbon-img {
+    float: left;
+    margin-right: 1rem;
+    border: 1px solid $borderColor;
+
+    img {
+      display: block;
+    }
+  }
+
+  .carbon-poweredby {
+    color: #999;
+    display: block !important;
+    margin-top: 0.5em;
+  }
+}
+</style>
